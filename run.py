b224943de13756022d4faaf47dc13a21b1d6fc1d
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2018-03-26 19:29:29 +0200

    Add base fule to run the program

---
+++ run.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 26.03.2018 19:17
+:Licence GNUv3
+Part of regexp-inverse
+
+"""
+
+from regexp_inverse import *
+
+if __name__ == '__main__':
+    (options, args) = cmd_parser.parse_args()
+    if len(args) != 1:
+        print('Invalid count of arguments')
+        exit(1)
+    generate(args[0], options.max_length, options.iterations, options.fill)
+    exit(0)
